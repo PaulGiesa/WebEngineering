4dad55f7522736ed5d162022f4fa7030b03946ba
PaulGiesa <pau.giesa@arcor.de>
2017-06-30 12:35:54 +0200

    Update

--- server.js
+++ server.js
@@ -30,7 +30,20 @@ app.get('/', function(req, res) {
 
 // API ROUTES -------------------
 // we'll get to these in a second
+// get an instance of the router for api routes
+var apiRoutes = express.Router();
 
+// TODO: route to authenticate a user (POST http://localhost:8080/api/authenticate)
+
+// TODO: route middleware to verify a token
+
+// route to show a random message (GET http://localhost:8080/api/)
+apiRoutes.get('/', function(req, res) {
+  res.json({ message: 'Welcome to the coolest API on earth!' });
+});
+
+// apply the routes to our application with the prefix /api
+app.use('/api', apiRoutes);
 // =======================
 // start the server ======
 // =======================
