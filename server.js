0f2ddad58b6fb982c4496fb5060948d09d11d7d9
PaulGiesa <pau.giesa@arcor.de>
2017-06-30 11:35:36 +0200

    update server.js

--- server.js
+++ server.js
@@ -1,39 +1,17 @@
-const http = require('http');
-const hostname = '127.0.0.1';
-const port = 3000;
 var express = require('express');
 var app = express();
-var bodyParser= require("body-parser")
 
-const server = http.createServer((req, res) => {
-  console.log (req.url);
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World\n');
-  });
+app.get('/', function (req, res) {
+   res.send('Hello World');
+})
 
-server.listen(port,hostname, ()  => {
-  console.log(`server running at http://${hostname}:${port}/`);
-  console.log(__dirname);
-  });
+app.put('/home', function (req, res) {
+   res.send('got a set');
+})
 
-//Readfilesync für user.js einlesen
-//PW ändern Json datei zur laufzeit generienren
-//nützliche website postman
+var server = app.listen(3000, function () {
+   var host = server.address().address
+   var port = server.address().port
 
-app.use(bodyParser.json());
-
-  app.get('/', function(req, res){
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Got a get request')
-});
-
-app.post('/', function(req, res){
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Got a Post request')
-});
-
-app.put('/', function(req, res){
-  res.setHeader('Content-Type', 'text/plain');
-  res.send('Got a Put request')
-});
+   console.log("Example app listening at http://%s:%s", host, port)
+})
