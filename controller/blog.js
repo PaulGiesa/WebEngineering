a79ba898cdccc09b49654b2c05d423eccd52c614
EluTodti <lilchris1310@aol.com>
2017-07-20 00:12:40 +0200

    Implemented delete blog

--- controller/blog.js
+++ controller/blog.js
@@ -1,33 +1,29 @@
 var route = require('../controller/routes.js');
 var blog = require('../model/blog.json');
+var fs = require('fs');
 
-function hidden_no_auth(req, res){
-  if(!blog[req.params.id]){       //blog id doesn't exist
-    route.not_found(req, res);
-    return;
-  }
-
-  if(!isAuthenticated && blog[req.params.id].hidden){  //not accessible -> hidden = true
-    route.not_authorized_401(req, res);
-    return;
-  }
-}
 
 
 //[req.params.id] used for index array
 exports.get_b = function (req, res){
-  hidden_no_auth(req,res);
-
   res.json(blog[req.params.id]);  //send blog
 }
 
 exports.delete_b = function (req, res){
-  hidden_no_auth(req, res);
-  //TODO
-  
+  delete blog[req.params.id];
+  fs.writeFile('./model/blog.json', JSON.stringify(blog), 'utf-8', (err) => {
+    if (err) {
+      res.status(500).json({error: err});
+    }
+    else{
+      res.status(200).send({
+            success: true ,
+            message: 'Page deleted'
+      });
+    }
+  })
 }
 
 exports.edit_b = function (req, res){
-  hidden_no_auth(req, res);
-  //TODO
+
 }
